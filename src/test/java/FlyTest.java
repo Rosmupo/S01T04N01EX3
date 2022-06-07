a7775c02afd0a72743c59cf2a94283618364c0d2
Rosmupo <95884239+Rosmupo@users.noreply.github.com>
2022-06-07 16:41:31 +0200

    completo

--- src/test/java/FlyTest.java
+++ src/test/java/FlyTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FlyTest {
-//Se crea un test que comprueba que la excepción que lanza el método trowMyExeption sea igual a la esperada.
+//Se crea un test que comprueba que la excepción que lanza el método trowMyException sea igual a la esperada.
     @Test
     void testCheckException() throws Exception {
         Fly fly = new Fly("");
